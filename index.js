25467134415c58af2addc28fb203abd729ac2648
denisc911 <denis.cabrera1990@gmail.com>
2024-07-08 20:12:08 +0200

    primer subida sin testear

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDB = require('./config');
+require('dotenv').config();
+
+const app = express();
+
+// Conectar a la base de datos
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Rutas
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/posts', require('./routes/postRoutes'));
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
